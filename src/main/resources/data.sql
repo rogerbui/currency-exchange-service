2b5b351b839cf737f1473a546ae8767100de5cf8
rogerbui <rogerbui@swissmarketplace.group>
2022-05-20 08:40:52 +0700

    currency service

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple)
+values(10001,'EUR', 'INR', 65);
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple)
+values(10002,'USD', 'INR', 75);
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple)
+values(10003,'AUD', 'INR', 25);
